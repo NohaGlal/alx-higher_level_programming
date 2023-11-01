df6d3b611a49f799cb4ba61f1ec90e424ca952bf
NohaGlal <engnohy@gmail.com>
2023-10-31 17:02:27 -0700

    sdsdd

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(9):
+    for m in range(i + 1, 10):
+     if i < m:
+         print("{:d}{:d}".format(i, m), 
+              end="\n" if i == 8 and m == 9 else ", ")

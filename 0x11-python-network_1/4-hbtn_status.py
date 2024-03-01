8098ce0534f33442bd73205a7d86578ba17f149f
NohaGlal <engnohy@gmail.com>
2024-03-01 10:21:37 -0800

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,13 @@
 import requests
 
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+def main():
+    """Entry point"""
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
+
+
+if __name__ == '__main__':
+    main()
